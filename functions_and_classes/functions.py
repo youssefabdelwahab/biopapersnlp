194c48971df14332d3b21ecf327580f89893b92e
LongLab1 <youssefabdelwahab10@gmail.com>
2025-04-12 16:32:52 -0600

    disabled pdf warnings

--- functions_and_classes/functions.py
+++ functions_and_classes/functions.py
@@ -24,6 +24,7 @@ logging.basicConfig(
         logging.StreamHandler()  
     ]
 )
+logging.getLogger("pdfminer").setLevel(logging.ERROR)
 
 USER_AGENTS = [
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36",
